cc832a69213f957969f39ecbba9eb00b5453b789
ShaharAizikovitz <shahar.haifa@gmail.com>
2024-02-17 12:42:47 +0200

    change to cmake chapter 2 to 10

---
+++ Ch10_Bitfields_Unions/header/strong_Typedef.hpp
@@ -0,0 +1,21 @@
+//====================================================================
+// File:        strong_Typedef.hpp
+// Description: Declaration of strongTypeDef class
+//====================================================================
+
+#pragma once
+
+// Place your templated strongTypeDef class definition here
+
+
+
+
+// Place your dummy structs for Length and Area types here
+
+
+
+
+// Place your typedefs defining Length and Area alias types here
+
+
+
